186f5c289af5ac0b3313dcaf1188feec0c5867c8
Alex J <abel.oalex@gmail.com>
2020-06-24 20:57:35 -0400

    Added mutation to ingredients

--- src/components/ingredientsPage.tsx
+++ src/components/ingredientsPage.tsx
@@ -1,57 +1,94 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import {gql} from "apollo-boost";
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import {Query, QueryResult} from 'react-apollo';
 
-interface typeOfData {
-    ingredients: {
-        _id: string,
-        name: string
-    }[]
-}
-
-interface Props {
-    ingredients: {
-        _id: string,
-        name: string
-    }[]
+interface IngredientsData {
+    ingredients: Ingredient[]
 }
 
 interface Ingredient {
     _id: string;
     name: string;
+    measure_unit: string;
 }
+
+interface theInput {
+    name: string;
+    measureUnit: string;
+}
+
+const GET_INGREDIENTS = gql`
+    {
+        ingredients {
+            _id,
+            name
+        }
+    }
+`;
+
+const SAVE_INGREDIENT = gql`
+    mutation createIngredient($input: CreateIngredientInput!) {
+        createIngredient(createIngredientInput: $input) {
+            name
+        }
+    }
+`;
 
 
 const IngredientsPage = (props) => {
 
-    const [ingredients , setIngredients] = useState([]);
-
-    const theQuery = gql`
-        {
-            ingredients {
-                _id,
-                name
+    //const [ingredients , setIngredients] = useState([]);
+    const [name, setName] = useState('');
+    const [measureUnit, setMeasureUnit] = useState('');
+    const [createIngredient, savedIngredient] = useMutation<
+        { createIngredient: Ingredient },
+        { input: theInput }
+        >(SAVE_INGREDIENT, {
+            variables: {
+                input: {
+                    name,
+                    measureUnit
+                }
             }
-        }
-    `;
+    });
+
+    const { loading, data } = useQuery<IngredientsData>( GET_INGREDIENTS );
 
     return (
         <div>
             <h1>The Ingredients Page</h1>
-            <Query query={theQuery}>
-                {
-                    (something: QueryResult<typeOfData>) => {
-                        if(!something.loading)
-                            setIngredients(something.data.ingredients);
-                        return null;
-                    }
-                }
-            </Query>
             <ul>
-                {ingredients.map(ingredient => <li>{ingredient.name} - {ingredient._id}</li>)}
+                {
+                    loading ?
+                        <p>Loading...</p> :
+                        data.ingredients.map(ingredient => <li key={ingredient._id}>{ingredient.name} - {ingredient._id}</li>)
+                }
             </ul>
+            <h3>Add Ingredient</h3>
+            {savedIngredient.error ? <p>Oh no! {savedIngredient.error}{console.log(savedIngredient.error)}</p> : null}
+            {savedIngredient.data && savedIngredient.data.createIngredient ? <p>Saved!</p> : null}
+            <form>
+                <p>
+                    <label>Name:</label>
+                    <input
+                        name="name"
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        name="measure_unit"
+                        onChange={e => setMeasureUnit(e.target.value)}
+                    />
+                </p>
+                <button onClick={(event) => {
+                    event.preventDefault();
+                    return createIngredient();
+                }}>
+                    Add
+                </button>
+            </form>
         </div>
     )
-}
+};
 
 export default IngredientsPage;
